fe11ccf602d126d7128e9599380f7158ac334569
Lucas Berchtold <lucas.berchtold@tum.de>
2020-03-25 16:36:35 +0100

    fix replanning: deactivate makeExperienceMobSimCompatible

--- src/main/scala/beam/replanning/SwitchChargingPreferences.scala
+++ src/main/scala/beam/replanning/SwitchChargingPreferences.scala
@@ -13,11 +13,12 @@ class SwitchChargingPreferences @Inject()(config: Config) extends PlansStrategyA
     var previousChargeSelection = 0
     if (person.getSelectedPlan.getAttributes.getAttribute("chargeAtActivity") != null)
       previousChargeSelection = person.getSelectedPlan.getAttributes.getAttribute("chargeAtActivity").asInstanceOf[Int]
-    ReplanningUtil.makeExperiencedMobSimCompatible(person)
+    //ReplanningUtil.makeExperiencedMobSimCompatible(person)
     ReplanningUtil.copyPlanAndSelectForMutation(person.getSelectedPlan.getPerson)
 
-    person.getSelectedPlan.getAttributes.clear()  // ! May cause some problems
+    //person.getSelectedPlan.getAttributes.clear()  // ! May cause some problems
+    person.getSelectedPlan.getAttributes.removeAttribute("chargeAtActivity")
     person.getSelectedPlan.getAttributes.putAttribute("chargeAtActivity", previousChargeSelection + 1)
-    log.debug("After Replanning ClearModes: Person-" + person.getId + " - " + person.getPlans.size())
+    log.debug("After Replanning SwitchChargingPreferences: Person-" + person.getId + " - " + person.getPlans.size())
   }
 }
