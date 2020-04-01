88f0f8cd64dfee3584f7bd36ca19f8486ac998fd
Lucas Berchtold <lucas.berchtold@tum.de>
2020-04-01 09:53:23 +0200

    cleanup

--- src/main/python/ftm/charging_power_calculation.py
+++ src/main/python/ftm/charging_power_calculation.py
@@ -2,23 +2,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def calcAvgChargingPowerNumeric(currentEnergyLevelInJoule, batteryCapacityInJoule, maxChargingPowerInKw, chargingLimit,
-                                stepSize):
-    updatedEnergyLevelInJoule = currentEnergyLevelInJoule
+def calc_avg_charging_power_numeric(start_energy_in_J, batteryCapacityInJoule, maxChargingPowerInKw, chargingLimit,
+                                    stepSize, return_plot_values=False):
+    updatedEnergyLevelInJoule = start_energy_in_J
     currentTimestep = 1
-    xvals = np.array([currentTimestep])
-    yvals = np.array([updatedEnergyLevelInJoule])
+    if return_plot_values:
+        xvals = np.array([currentTimestep])
+        yvals = np.array([updatedEnergyLevelInJoule])
     while (updatedEnergyLevelInJoule < chargingLimit):
         currentChargingPowerInKw = (0.6 * 0.05 ** (
                 updatedEnergyLevelInJoule / batteryCapacityInJoule) + 0.4) * maxChargingPowerInKw
         stepEnergyInJoule = stepSize * currentChargingPowerInKw * 1000
         currentTimestep += stepSize
         updatedEnergyLevelInJoule += stepEnergyInJoule
-        xvals = np.append(xvals, [currentTimestep])
-        yvals = np.append(yvals, [updatedEnergyLevelInJoule])
+        if return_plot_values:
+            xvals = np.append(xvals, [currentTimestep])
+            yvals = np.append(yvals, [updatedEnergyLevelInJoule])
 
-    avgChargingPowerInKw = (chargingLimit - currentEnergyLevelInJoule) / currentTimestep / 1000
-    return (avgChargingPowerInKw, xvals, yvals)
+    avgChargingPowerInKw = (chargingLimit - start_energy_in_J) / currentTimestep / 1000
+    if return_plot_values:
+        return (avgChargingPowerInKw, xvals, yvals)
+    else:
+        return avgChargingPowerInKw
 
 
 def plot_charging_power_over_energy(e_max_in_j, p_max_in_kw):
@@ -36,7 +41,7 @@ def plot_charging_power_over_energy(e_max_in_j, p_max_in_kw):
     plt.show()
 
 
-
+"""
 E_max_in_J = 2.7e8
 P_max_in_kW = 7.3
 # Plot difference between linear and nonlinear charging
@@ -48,14 +53,14 @@ maxChargingPowerInKw = 7.2
 stepSize = 100
 fig, ax = plt.subplots()
 
-(avgChargingPowerInKw, xvals, yvals) = calcAvgChargingPowerNumeric(currentEnergyLevelInJoule, batteryCapacityInJoule, maxChargingPowerInKw, chargingLimit, stepSize)
+(avgChargingPowerInKw, xvals, yvals) = calc_avg_charging_power_numeric(currentEnergyLevelInJoule, batteryCapacityInJoule, maxChargingPowerInKw, chargingLimit, stepSize)
 sessionLengthInS = (chargingLimit - currentEnergyLevelInJoule) / 3.6e6 / avgChargingPowerInKw * 3600.0
 print(sessionLengthInS)
 ax.plot(xvals/3600, yvals/3.6e6, label='NonLinear: Stepsize '+str(stepSize))
 ax.plot([0, sessionLengthInS/3600], [currentEnergyLevelInJoule/3.6e6, chargingLimit/3.6e6], label='NonLinear avg: Stepsize '+str(stepSize))
 
 smallStepSize = 1
-(avgChargingPowerInKwSmallStep, xvals, yvals) = calcAvgChargingPowerNumeric(currentEnergyLevelInJoule, batteryCapacityInJoule, maxChargingPowerInKw, chargingLimit, smallStepSize)
+(avgChargingPowerInKwSmallStep, xvals, yvals) = calc_avg_charging_power_numeric(currentEnergyLevelInJoule, batteryCapacityInJoule, maxChargingPowerInKw, chargingLimit, smallStepSize)
 sessionLengthInSSmallStep = (chargingLimit - currentEnergyLevelInJoule) / 3.6e6 / avgChargingPowerInKwSmallStep * 3600.0
 ax.plot(xvals/3600, yvals/3.6e6, label='NonLinear: Stepsize '+str(smallStepSize))
 ax.plot([0, sessionLengthInSSmallStep/3600], [currentEnergyLevelInJoule/3.6e6, chargingLimit/3.6e6], label='NonLinear avg: Stepsize '+str(smallStepSize))
@@ -76,3 +81,4 @@ ax.set_xlabel('Charging duration in h')
 ax.set_ylabel('SOC in kWh')
 ax.legend()
 plt.show()
+"""
