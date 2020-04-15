ea85fb7275526330ed979f0f8cef33f59b5ed686
Lucas Berchtold <lucas.berchtold@tum.de>
2020-04-15 07:58:38 +0200

    chargeAtActivity refactoring and add methods to modify charging sequence

--- src/main/scala/ftm/util/PopulationUtil.scala
+++ src/main/scala/ftm/util/PopulationUtil.scala
@@ -16,7 +16,7 @@ object PopulationUtil {
     val chargeAtActivity = random.nextInt(numberOfCombinations + 1)
     //ReplanningUtil.makeExperiencedMobSimCompatible(person)
     ReplanningUtil.copyPlanAndSelectForMutation(person.getSelectedPlan.getPerson)
-    person.getSelectedPlan.getAttributes.clear()
+    person.getSelectedPlan.getAttributes.removeAttribute("chargeAtActivity")
     person.getSelectedPlan.getAttributes.putAttribute("chargeAtActivity", chargeAtActivity)
   }
 
@@ -32,11 +32,10 @@ object PopulationUtil {
     //val combinations = scala.math.pow(2, matsimPlan.getPlanElements.size() - 1).toInt
     val numberOfCombinations = Math.pow(2, getNumberOfActivities(plan)-1).toInt
     val chargeAtActivity = plan.getAttributes.getAttribute("chargeAtActivity").asInstanceOf[Int]
-    (0x100 + chargeAtActivity).toBinaryString.tail.map{ case '1' => true; case _ => false }.reverse
+    (numberOfCombinations + chargeAtActivity).toBinaryString.tail.map{ case '1' => true; case _ => false }.reverse
   }
 
-  def chargeAtActivityBooleanSeqToString(booleanSeq: IndexedSeq[Boolean]): String = {
-    val reverse = booleanSeq.reverse
+  def chargeAtActivityBooleanSeqToString(booleanSeq: IndexedSeq[Boolean]): Int = {
     var sum = 0.0
     booleanSeq.zipWithIndex.foreach {
       case (value, index) => {
@@ -45,6 +44,51 @@ object PopulationUtil {
       }
       case _ =>
     }
-    sum.toString
+    sum.toInt
+  }
+
+  def addChargingActivityToChargingSequence(sequence: IndexedSeq[Boolean]): IndexedSeq[Boolean] = {
+    // Check if we are already charging at every activity
+    var newSequence = sequence
+    if (sequence.filter(value => value).size < sequence.size) {
+      var change = false
+      while (!change) {
+        val randomIndex = Random.nextInt(sequence.length)
+        if (!sequence(randomIndex)) {
+          newSequence = sequence.updated(randomIndex, true)
+          change = true
+        }
+      }
+    }
+    newSequence
+  }
+
+  def removeChargingActivityFromChargingSequence(sequence: IndexedSeq[Boolean]): IndexedSeq[Boolean] = {
+    // Check if we have a charging activity to remove
+    var newSequence = sequence
+    if (sequence.filter(value => value).size > 0) {
+      var change = false
+      while (!change) {
+        val randomIndex = Random.nextInt(sequence.length)
+        if (sequence(randomIndex)) {
+          newSequence = sequence.updated(randomIndex, false)
+          change = true
+        }
+      }
+    }
+    newSequence
+  }
+
+  def moveChargingActivityInChargingSequence(sequence: IndexedSeq[Boolean]): IndexedSeq[Boolean] = {
+    // Check if we have a charging activity to move
+    var newSequence = sequence
+    if (sequence.filter(value => value).size > 0) {
+      // make sure the sequence has changed
+      while(newSequence == sequence) {
+        newSequence = removeChargingActivityFromChargingSequence(newSequence)
+        newSequence = addChargingActivityToChargingSequence(newSequence)
+      }
+    }
+    newSequence
   }
 }
