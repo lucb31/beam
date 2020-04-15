0c3ef41e6c8e6cc64118c7386e89f99f5c9563c7
Lucas Berchtold <lucas.berchtold@tum.de>
2020-04-15 07:49:36 +0200

    refactor, add average line, plot every 5th iteration

--- src/main/python/ftm/plot_charger_utilization.py
+++ src/main/python/ftm/plot_charger_utilization.py
@@ -2,26 +2,16 @@ import pandas as pd
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
-from python.ftm.plot_events import get_refuel_events_from_events_csv
-from python.ftm.util import get_run_dir, get_latest_run, get_iteration_dir
+from python.ftm.plot_events import get_refuel_events_from_events_csv, get_total_walking_distances_from_events_csv, \
+    get_parking_events_from_events_csv
+from python.ftm.util import get_run_dir, get_latest_run, get_iteration_dir, get_last_iteration
 
-# Configuration
-baseDir = "/home/lucas/IdeaProjects/beam/output/munich-simple/"
-latest_run = get_latest_run(baseDir)
-run_dir = get_run_dir(baseDir, latest_run)
-last_iteration = 3
-num_rows = int(last_iteration / 2) + 1
-num_cols = last_iteration % 2 + 1
 
-plotly_figure = make_subplots(rows=last_iteration+1, cols=1, subplot_titles=("Iteration 0", "Iteration 1", "Iteration 2", "Iteration 3"), shared_xaxes=True, vertical_spacing=0.02)
-# Get parking events from events.xml
-for iteration in range(last_iteration + 1):
+def plot_iteration(iteration, row, col):
     # Setup directory
     working_dir = get_iteration_dir(run_dir, iteration)
     print("-------------- ITERATION", iteration, "--------------------")
     print("Working on path: ", working_dir)
-    row = iteration + 1             # Stacked layout
-    col = 1                         # Stacked layout
 
     df_events_refueling = get_refuel_events_from_events_csv(working_dir + "events.csv")
     size_before_filtering = len(df_events_refueling.index)
@@ -30,7 +20,6 @@ for iteration in range(last_iteration + 1):
     print("Total of ", len(df_events_refueling.index), "Refuel Events")
 
     # Group by charger
-    df_chargers = df_events_refueling.groupby('parkingTaz')
     x = pd.Series()
     y = pd.Series()
     df_refueling_sum = df_events_refueling.groupby('parkingTaz').sum()
@@ -47,14 +36,54 @@ for iteration in range(last_iteration + 1):
         row=row,
         col=col
     )
+    plotly_figure.add_trace(
+        go.Line(
+            x=pd.Series([0, x.max()]),
+            y=pd.Series([y.mean(), y.mean()]),
+            name="It " + str(iteration) + " Average"
+        ),
+        row=row,
+        col=col
+    )
 
-ytitle = "# of charging events"
+    # Print parking events
+    df_parking = get_parking_events_from_events_csv(working_dir + "events.csv")
+    df_parking_group = df_parking.groupby('parkingTaz')
+    for name, group in df_parking_group:
+        print("TAZ ", name, "has ", len(group.index), "parking events")
+
+# Configuration
+baseDir = "/home/lucas/IdeaProjects/beam/output/munich-simple/"
+latest_run = get_latest_run(baseDir)
+#latest_run = "munich-simple__2020-04-06_11-04-37"
+run_dir = get_run_dir(baseDir, latest_run)
+last_iteration = 11
+step_size = 5
+compare_iterations = True
 ytitle = "transferred energy in kwh"
-plotly_figure['layout']['xaxis4'].update(title="Charging TAZ")
-plotly_figure['layout']['yaxis1'].update(title=ytitle)
-plotly_figure['layout']['yaxis2'].update(title=ytitle)
-plotly_figure['layout']['yaxis3'].update(title=ytitle)
-plotly_figure['layout']['yaxis4'].update(title=ytitle)
+
+if compare_iterations:
+    num_rows = int(last_iteration / step_size) + 1
+    if last_iteration % step_size > 0:
+        num_rows += 1
+    subplot_titles = ["Iteration "+str(iteration) for iteration in range(0, last_iteration, step_size)]
+    subplot_titles.append("Iteration "+str(last_iteration))
+    plotly_figure = make_subplots(rows=num_rows, cols=1, subplot_titles=subplot_titles, shared_xaxes=True, vertical_spacing=0.02)
+    row = 1
+    for iteration in range(0, last_iteration, step_size):
+        plot_iteration(iteration, row, 1)
+        plotly_figure['layout']['yaxis'+str(row)].update(title=ytitle)
+        row += 1
+
+    plot_iteration(last_iteration, row, 1)
+    plotly_figure['layout']['yaxis'+str(row)].update(title=ytitle)
+    plotly_figure['layout']['xaxis'+str(row)].update(title="Charging TAZ")
+    plotly_figure['layout']['xaxis'+str(row)].update(range=[100, 700])
+
+else:
+    last_iteration = get_last_iteration(run_dir)
+    plotly_figure = make_subplots(rows=1, cols=1, subplot_titles=("Iteration " + str(last_iteration), ""), shared_xaxes=True, vertical_spacing=0.02)
+    plot_iteration(last_iteration, 1, 1)
 plotly_figure.update_layout(
     title_text="Charger utilization for simulation "+latest_run
 )
